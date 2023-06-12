2912484df7a90b95ef00ed478e3c118af7a54841
Zakiir Juman <zakiirjuman@gmail.com>
2023-06-12 16:57:53 -0400

    Create main routes.

--- app.js
+++ app.js
@@ -11,9 +11,39 @@ const { DB_HOST, DB_PORT, DB_NAME } = process.env;
   const { db } = await createConnection(DB_HOST, DB_PORT, DB_NAME);
   if (db) {
     //ensure that the players collection exists
-    //app.get("/players", async (req, res) => {});
-    //app.get("/toronto", async (req, res) => {});
-    //app.get("/points", async (req, res) => {});
+    const players = db.collection("nhl_stats_2022");
+
+    //error if there are no documents in the collection
+    if ((await players.countDocuments({})) === 0) {
+      console.log("No documents in players collection   ...exiting");
+      process.exit(1);
+    }
+
+    //return all documents in the nhl_stats_2022 collection
+    app.get("/players", async (req, res) => {
+      const cursor = await players.find({});
+      const results = await cursor.toArray();
+      res.json(results);
+    });
+    //return top players
+    app.get("/players/top/:number", async (req, res) => {
+      //convert the number to an integer
+      const number = parseInt(req.params.number);
+      const cursor = await players.find({}).sort({ Pts: -1 }).limit(number);
+      const results = await cursor.toArray();
+      res.json(results);
+    });
+    //returns all players with teamName
+    app.get("/players/team/:teamName", async (req, res) => {
+      const cursor = await players.find({ Team: req.params.teamName });
+      const results = await cursor.toArray();
+      res.json(results);
+    });
+    //list all team names
+    app.get("/teams", async (req, res) => {
+      const cursor = await players.distinct("Team");
+      res.json(cursor);
+    });
     app.listen(3000, () => {
       console.log("Server started on port 3000");
     });
---
+++ import.sh
@@ -0,0 +1,9 @@
+# Specify the MongoDB connection details
+MONGO_HOST="localhost"
+MONGO_PORT="27017"
+MONGO_DB="WCD_project2"
+COLLECTION_NAME="nhl_stats_2022"
+CSV_FILE="nhl-stats-2022.csv"
+
+# Use mongoimport to import the CSV file into the specified MongoDB collection
+mongoimport --host $MONGO_HOST --port $MONGO_PORT --db $MONGO_DB --collection $COLLECTION_NAME --type csv --headerline --file $CSV_FILE
